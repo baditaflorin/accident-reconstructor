0f17f6de9e0b978838991d70af54461891f17ab0
Florin Badita-Nistor <baditaflorin@gmail.com>
2026-05-08 02:27:43 +0300

    ops: refresh published version metadata

--- src/generated/version.ts
+++ src/generated/version.ts
@@ -1,3 +1,3 @@
 export const APP_VERSION = "0.1.0"
-export const APP_COMMIT = "514b663"
-export const APP_BUILT_AT = "2026-05-07T23:21:17.439Z"
+export const APP_COMMIT = "3887254"
+export const APP_BUILT_AT = "2026-05-08T02:25:02+03:00"
